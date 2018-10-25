fe6c1766eb117a4178288cf200f1fbf954de3cc1
Danielle Dookie <danielledookie90@gmail.com>
2018-10-25 01:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+function destructivelyAppendDriver(name) {
+  drivers.pop()
+}
+
+function destructivelyAppendDriver(name) {
+  drivers.shift()
+}
+
+function appendDriver(name) {
+  const driversCopy = drivers.slice()
+  driversCopy.push(name)
+  return driversCopy
+}
+
+function prependDriver(name) {
+  const driversCopy = drivers.slice()
+  driversCopy.unshift(name)
+  return driversCopy
+}
+
+function removeLastDriver() {
+  const driversCopy = drivers.slice()
+  driversCopy.pop()
+  return driversCopy
+}
+
